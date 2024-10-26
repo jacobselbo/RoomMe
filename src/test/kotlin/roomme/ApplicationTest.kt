574413bb01f9507afebe4510ab5c5c99abd4c74d
jacobselbo <selbo.a.jacob@gmail.com>
2024-10-26 10:30:31 -0400

    Initial commit

---
+++ src/test/kotlin/roomme/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package roomme
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import roomme.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
