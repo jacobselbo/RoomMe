b57f4967b10ab9b6b04b68e134ffc4fe55a06720
Lemon-Chad <40183264+Lemon-Chad@users.noreply.github.com>
2024-10-28 12:34:48 -0400

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-kill -9 $(lsof -t -i :7449)
-export mongodbPassword=b3usYGjMzx9od4tj
-export sessionEncryptKey=809d15f0691242f4fc18d28f53c1a1f3
-export sessionSignKey=cf84f04c86c2739aebceae2fcb190b52
-java -jar RoomMe-all.jar
